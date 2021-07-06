ac74f5ef90580859cdc1474dab6bb31f712b5bad
+ <+>
2021-07-06 17:19:56 -0400

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let multiply = num1 * num2, num1 = 31, num2 = 2;
+
+multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+let num3 = 14, num4 = 10;
+let mod = num3 % num4;
+
+let max = Math.max(1, 3, 20);
